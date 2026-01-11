b32b01c8b6584963339eeefce3a91e65a6d2e960
yctan1993 <yongchintan93@gmail.com>
2026-01-11 09:40:52 +0800

    Corrected syntax for csv to map object conversion

--- 01-iam.tf
+++ 01-iam.tf
@@ -1,16 +1,30 @@
 
 locals {
-    iam_test_users = { for iam_user in csvdecode(file("./iam_onboarding/users/test_users/iam-users.csv")) : iam_user.name => iam_user}
-    iam_test_groups = { for iam_group in csvdecode(file("./iam_onboarding/groups/test_groups/iam-groups.csv")) : iam_group.name => iam_group}
-    iam_test_groups_memberships = { for iam_group_membership in csvdecode(file("./iam_onboarding/groups_memberships/iam-groups-memberships.csv")) : iam_group_membership.group_name => {
-                                        group_name = iam_group_membership.group_name
-                                        /* 
-                                            Converts the users column from string to list(string)
-                                            trimspace removes the white spaces in the string
-                                        */
-                                        users = [for user in split(",", iam_group_membership.users) : trimspace(user)] 
+    iam_test_groups =   { for iam_group in csvdecode(file("./iam_onboarding/groups/test_groups/iam-groups.csv")) : iam_group.name => {
+                                name = iam_group.name
+                                path = iam_group.path == "" ? null : iam_group.path
+                            }
+                        }
+
+
+    iam_test_users =    { for iam_user in csvdecode(file("./iam_onboarding/users/test_users/iam-users.csv")) : iam_user.name => {
+                                name = iam_user.name
+                                path = iam_user.path == "" ? null : iam_user.path
+                                permissions_boundary = iam_user.permissions_boundary == "" ? null : iam_user.permissions_boundary
+                                force_destroy = iam_user.force_destroy
+                                create_access_key = iam_user.create_access_key
+                            }
+                        }
+
+    iam_test_groups_memberships =   { for iam_group_membership in csvdecode(file("./iam_onboarding/groups_memberships/iam-groups-memberships.csv")) : iam_group_membership.group_name => {
+                                                group_name = iam_group_membership.group_name
+                                                /* 
+                                                    Converts the users column from string to list(string)
+                                                    trimspace removes the white spaces in the string
+                                                */
+                                                users = [for user in split(",", iam_group_membership.users) : trimspace(user)] 
+                                            }
                                     }
-                                  }
 }
 
 module "iam_test_groups" {
